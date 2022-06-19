00e8042d4c98df9b673c3d3ade48d03ff1805faa
bisza <103103412+rattanasak1998@users.noreply.github.com>
2022-06-19 15:22:06 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FormComponent from './components/FormComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent/>
+    </div>
+  );
+}
+
+export default App;
